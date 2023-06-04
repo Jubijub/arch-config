6fc2e1bea88feaae8443efdca855ea623d198cc4
Julien Chappuis <jubijub@gmail.com>
2023-06-05 00:03:24 +0200

    Adds neovim config files

---
+++ home/jubi/.config/nvim/lua/jubi/plugins/telescope-fzf-native.lua
@@ -0,0 +1,5 @@
+return {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build =
+    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+}
---
+++ home/jubi/.config/nvim/lua/jubi/remap.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Allows to move selected text up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- cursor stays in place when appending the line below
+vim.keymap.set("n", "J", "mzJ`z")
+-- cursor stays in the middle during half page jumps
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- search term stays in the middle during searches
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- pasting foo over bar keeps foo in the buffer
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Copies to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- LSP formatting
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
