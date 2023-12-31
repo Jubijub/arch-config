281a6ec73eb2224885dd59d5e38a758d718667a0
Julien Chappuis <jubijub@gmail.com>
2023-12-31 12:13:15 +0100

    fix: better copilot-status icons

--- home/jubi/.config/nvim/after/plugin/copilot-status.lua
+++ home/jubi/.config/nvim/after/plugin/copilot-status.lua
@@ -1,9 +1,9 @@
 require('copilot_status').setup({
     icons = {
-        idle = " ",
-        error = " ",
-        offline = " ",
-        warning = "𥉉 ",
+        idle = " ", --\uF254 in nonicons font
+        error = " ", --\uf256
+        offline = " ", --\uf255
+        warning = " ",
         loading = " ",
     },
     debug = false,
